76c6f364266f2d44451833b5aecee49d2db64873
savagegraphics <olawaleraji188@gmail.com>
2022-10-18 05:53:00 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import './App.css';
+import Pages from './components/pages/Pages';
+
+const App = () => {
+  return (
+    <div>
+      <Pages />
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+// import './App.css';
+// import NavBar from './component/firstContent/NavBar';
+// import Hero from './component/firstContent/Hero';
+// // import Features from './component/Features';
+// import Footer from './component/firstContent/Footer';
+// import HeroTwo from './component/firstContent/HeroTwo';
+// import HeroSection from './component/firstContent/HeroSection';
+// import Products from './component/firstContent/Products';
+// import Another from './component/firstContent/Another';
+// // import HeroSection from './component/HeroSection';
+
+// //import Images
+
+// // import FtImage1 from './assets/feature1.jpg'
+// // import FtImage2 from './assets/feature2.jpg'
+// // import FtImage3 from './assets/googlegift.png'
+// // import Savage from './assets/savage.JPG'
+// // import HeroBg from './assets/bgimage.jpg'
+
+
+
+
+// function App() {
+//   return (
+//     <>
+//       <NavBar />
+//       <Hero />
+//       <HeroSection />
+//       <HeroTwo />
+//       <Another />
+//       <Footer />
+//       {/* <HeroSection /> */}
+
+
+//     </>
+//   );
+// }
+
+// export default App;
